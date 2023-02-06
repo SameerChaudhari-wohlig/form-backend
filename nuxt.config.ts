8dd5614e3837f57fc14f19fa8e3c251f2c5adc57
sameer <sameer.chaudhari@wohlig.com>
2023-02-06 18:28:59 +0530

    backend

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+export default defineNuxtConfig({
+  css: ["bootstrap/dist/css/bootstrap.min.css"],
+  script: [
+    {
+      src: "bootstrap/dist/js/bootstrap.bundle.min.js",
+    },
+  ],
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
+});
