b225d5f58d49861ea5f6cef041abf126ba100680
sameer <sameer.chaudhari@wohlig.com>
2023-02-07 18:07:17 +0530

    first commit

---
+++ pages/get.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app>
+    <v-app-bar title="Instagram" class="px-5">
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <v-text-field class="pad" size="1" rounded placeholder="Search" v-model="name"><v-btn icon="mdi-magnify"></v-btn>
+      </v-text-field>
+    </v-app-bar>
+    <!-- <pre>{{ Products }}</pre> -->
+    <v-main class="my-8">
+      <table class="table caption-top">
+
+        <thead>
+          <tr class="table-primary">
+            <th scope="col" >ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Age</th>
+            <th scope="col">BloodGroup</th>
+            <th scope="col">Gender</th>
+            <th scope="col">Subject</th>
+            <th scope="col">ranking</th>
+          </tr>
+          <tr class="table-info" v-for="Product in Products" :key="Product.id">
+            <td >{{ Product.id }}</td>
+            <td>{{ Product.name }}</td>
+            <td>{{ Product.age }}</td>
+            <td>{{ Product.Bloodgroup }}</td>
+            <td>{{ Product.gender }}</td>
+            <td>{{ Product.subject }}</td>
+            <td>{{ Product.ranking }}</td>
+          </tr>
+        </thead>
+      </table>
+
+      <!-- <v-row>
+        <v-col cols="6">
+          <v-card class="mx-auto" max-width="344">
+  
+            <h3 class="mt-4">{{ Product.name }}</h3>
+            <v-btn icon="mdi-cards-heart"></v-btn>
+            <v-btn icon="mdi-comment"></v-btn>
+            <v-btn icon="mdi-share"></v-btn>
+            <v-card-title>
+              UserName : {{ Product.age }}</v-card-title
+            >
+            <h3>{{ Product.gender }} </h3>
+            <v-expand-transition>
+              <div v-show="show">
+                <v-divider></v-divider>
+              </div>
+            </v-expand-transition>
+          </v-card>
+        </v-col>
+      </v-row> -->
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import data from "../data";
+const Products = ref(data);
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,119 @@
+<template>
+
+    <div class="dropdown-container d-flex mt-3 gap-5 ms-4 ">
+        <div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1"
+                data-bs-toggle="dropdown" aria-expanded="false">
+                Student: ID
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li v-for="Product in Products" :key="Product.id"><a class="dropdown-item" href="#">{{ Product.id }}</a>
+                </li>
+            </ul>
+        </div>
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Student: NAME
+            </button>
+            <ul class="dropdown-menu">
+                <li v-for="Product in Products" :key="Product.id"><a class="dropdown-item" href="#">{{
+                    Product.name
+                }}</a>
+                </li>
+            </ul>
+        </div>
+        <div class="dropdown">
+            <button class="btn btn-success dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Student: AGE
+            </button>
+            <ul class="dropdown-menu">
+                <li v-for="Product in Products" :key="Product.id"><a class="dropdown-item" href="#">{{
+                    Product.age
+                }}</a>
+                </li>
+            </ul>
+        </div>
+        <div class="dropdown">
+            <button class="btn btn-info dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Student: BLOOD GROUP
+            </button>
+            <ul class="dropdown-menu">
+                <li v-for="Product in Products" :key="Product.id"><a class="dropdown-item" href="#">{{
+    Product.Bloodgroup
+                }}</a>
+                </li>
+            </ul>
+        </div>
+        <div class="dropdown">
+            <button class="btn btn-warning dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Student: GENDER
+            </button>
+            <ul class="dropdown-menu">
+                <li v-for="Product in Products" :key="Product.id"><a class="dropdown-item" href="#">{{
+                    Product.gender
+                }}</a>
+                </li>
+            </ul>
+        </div>
+        <div class="dropdown">
+            <button class="btn btn-danger dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Student: SUBJECT
+            </button>
+            <ul class="dropdown-menu">
+                <li v-for="Product in Products" :key="Product.id"><a class="dropdown-item" href="#">{{
+                    Product.subject
+                }}</a></li>
+            </ul>
+        </div>
+        <div class="dropdown">
+            <button class="btn btn-info dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Student: Ranking
+            </button>
+            <ul class="dropdown-menu">
+                <li v-for="Product in Products" :key="Product.id"><a class="dropdown-item" href="#">{{
+                    Product.ranking
+                }}</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- carousels -->
+    <div id="carouselExampleInterval" class="carousel slide mt-3" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="5000">
+                <img src=" https://4kwallpapers.com/images/wallpapers/ducati-panigale-v4-sports-bikes-2022-red-bikes-dark-3840x2160-6952.jpg"
+                    class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item" data-bs-interval="4000">
+                <img src="https://images.unsplash.com/photo-1635073891744-1e8a4e9cbe98?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Ym13JTIwczEwMDBycnxlbnwwfHwwfHw%3D&w=1000&q=80"
+                    class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item" data-bs-interval="4000">
+                <img src="https://wallpaperaccess.com/full/4241928.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import data from "../data";
+const Products = ref(data);
+</script>
+
+<style>
+
+</style>
